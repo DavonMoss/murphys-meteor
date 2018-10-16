aa3f077f7b0f4784cd8fbc90fac34c10614806a1
Davon Moss <davonlmossNDHS@gmail.com>
2018-10-16 12:37:03 -1000

    Mockup (w/ template files)

---
+++ app/imports/ui/pages/Murphys.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import TopMenu from '../components/TopMenu.jsx';
+import Middle from '../components/Middle.jsx';
+import BottomMenu from '../components/BottomMenu.jsx';
+
+export default class IslandSnow extends React.Component {
+  render() {
+    return (
+      <div>
+        <Middle/>
+      </div>
+    );
+  }
+}
